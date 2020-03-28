62591dcd6b2b73ad4096cfab690dbd2fb1bd35a8
Adebisi Joseph <adebisijoe@gmail.com>
2020-03-28 01:02:48 +0100

    added Billing model and resource controller

--- database/migrations/2020_03_28_000008_create_billings_table.php
+++ database/migrations/2020_03_28_000008_create_billings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateBillingsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('billings', function (Blueprint $table) {
             $table->increments('id');
-            //$table->string('name');
-            $table->string('username')->unique();
-            $table->string('mobile_number');
-            $table->decimal('amount_to_bill', 18);
             $table->timestamps();
-
-
-            
         });
     }
 
@@ -33,6 +26,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('billings');
     }
 }
